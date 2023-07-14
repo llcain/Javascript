61dfb2c0deed6b3f75dddcd4e2328f5825d88c51
llcain <llcain2012@gmail.com>
2023-07-14 12:57:15 -0500

    Updated script js

--- callBack-script.js
+++ callBack-script.js
@@ -8,3 +8,40 @@ button.addEventListener('click', () => {
     let name = "Jon Doe";
     console.log(name.toUpperCase());
 });
+
+const users = [
+    {
+        firstName: 'Susan',
+        lastName: "Curry",
+        age: 14,
+        hobbie: "singing"
+    },
+    {
+        firstName: 'Bob',
+        lastName: "Bones",
+        age: 26,
+        hobbie: "bowling"
+    },
+    {
+        firstName: 'Tim',
+        lastName: "Jones",
+        age: 35,
+        hobbie: "painting"
+    },
+    {
+        firstName: 'Will',
+        lastName: "Charge",
+        age: 22,
+        hobbie: "knitting"
+    }
+];
+
+const output = users.map(({ age, hobbie, firstName  }) => {
+    return `<ul>
+    <li>${age}</li>
+    <li>${hobbie}</li>
+    <li>${firstName}</li>
+</ul>`
+});
+
+console.log(output);
